03cdb883c7cb594602bb99c998945c7e6bcc222e
Pete-in-Perth <30366062+Pete-in-Perth@users.noreply.github.com>
2017-10-30 21:04:07 +0800

    Add files via upload

---
+++ shutdown_pi.py
@@ -0,0 +1,44 @@
+#!/bin/python  
+# Script for restarting or shutting down the raspberry Pi by pressing or holding a button.  
+# by Peter Glorie
+
+# Button should be connected between ground and GPIO pin
+# Use of resistor on one of the button connections recommended to avoid accidental damage to pi.
+  
+import RPi.GPIO as GPIO  
+import time  
+import os  
+  
+# Use the Broadcom SOC Pin numbers  
+PIN = 21
+
+# Setup the Pin with Internal pullups enabled and PIN in reading mode.  
+GPIO.setmode(GPIO.BCM)  
+GPIO.setup(PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
+
+
+# Our function on what to do when the button is pressed  
+def Shutdown(channel):  
+	print("Shutdown initiated")
+	#os.system("sudo shutdown -h now")  
+
+def Reboot(channel):  
+	print("Reboot initiated")
+	#os.system("sudo reboot")  
+	
+def BTime(channel):
+	t = 0
+	while GPIO.input(channel) == GPIO.LOW:
+		time.sleep(10)
+		t += 10
+		if t > 2000: # if button is held long enough
+			Shutdown()
+	Reboot() # if button is released early
+	
+ 
+# Add our function to execute when the button pressed event happens  
+GPIO.add_event_detect(PIN, GPIO.FALLING, callback = BTime, bouncetime = 2000)  
+ 
+# Now wait!  
+while 1:  
+	time.sleep(1)  
