93ba1a7f0ce65b5fcaaf88dd89f2444178f82e1e
Pete-in-Perth <30366062+Pete-in-Perth@users.noreply.github.com>
2019-09-13 08:20:22 +0800

    Update wlan_monitor.sh

--- wlan_monitor.sh
+++ wlan_monitor.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # script for monitoring wireless connection and re-connecting if lost
-# source: https://www.raspberrypi.org/forums/viewtopic.php?t=16054#p165196
+# source: https://www.raspberrypi.org/forums/viewtopic.php?t=16054#p165196 and
+# https://www.raspberrypi.org/forums/viewtopic.php?t=178175
 
 while true ; do
 
 
-	ping -c4 1.1.1.1 > /dev/null
+	ping -c4 192.168.1.1 > /dev/null
 	    if [ $? != 0 ]
 	    then
 		echo "No network connection, restarting wlan0"
